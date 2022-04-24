a68071a2de9f1bc831bda67cbbb31fac5cd55cc5
Ananstasia Shulga <anastasiansh@gmail.com>
2022-04-24 23:45:51 +0400

    V-10 Верстка детальной страницы автомобиля.

---
+++ src/components/Car/ImageLoader.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="image-loader">
+    <div class="image-loader-file-input">
+      <v-file-input
+        prepend-icon=""
+        suffix="Обзор"
+        placeholder="Выберите файл..."
+        outlined
+        @change="uploadImage"
+      ></v-file-input>
+    </div>
+    <div
+      v-if="imgLoading"
+      class="image-loader-progress-bar"
+    >
+      <v-progress-linear
+        background-color="disabled"
+        color="primary"
+        height="5"
+        :buffer-value="bufferValue"
+        :value="value"
+        rounded
+      >
+        <template v-slot:default="{ value }">
+          <div> Заполнено <span>{{ Math.ceil(value) }}%</span></div>
+        </template>
+      </v-progress-linear>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    imgLoading: false,
+    bufferValue: 100,
+    value: 0,
+  }),
+  methods: {
+    iterate() {
+      if (this.bufferValue !== this.value) {
+        this.value++;
+      } else {
+        this.hideProgressBar();
+      }
+    },
+    hideProgressBar() {
+      this.imgLoading = false;
+      this.value = 0;
+    },
+    async uploadImage(event) {
+      if (event) {
+        this.imgLoading = true;
+        this.$emit("onUpload", event, () => this.iterate());
+      }
+    },
+  },
+};
+</script>
---
+++ src/views/Order/VOrder.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="admin-content">
+    <h1>Заказ №{{ this.$route.params.id }} </h1>
+    <v-container
+      class="admin-content__container d-flex flex-column elevation-5"
+      fluid
+    >
+
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    order: {
+      updatedAt: 1649615356071,
+      createdAt: 1649615356071,
+      cityId: {
+        name: "Санкт-Петербург",
+        id: "6005b8f9ad015e0bb6997778",
+      },
+      pointId: {
+        name: "Пункт",
+        address: "56 Литейный проспект",
+        id: "615ae47018f5c2264119b939",
+      },
+      carId: {
+        description:
+          "Пробег до 500 км на одном зараде. Отличный выбор для деловых поездок",
+        priceMin: 10000,
+        priceMax: 15000,
+        name: "Tesla Model S",
+        number: "А700АА",
+        categoryId: {
+          name: "Бизнес",
+          description: "Бизнес",
+          id: "611171dd2aed9a0b9b8506f9",
+        },
+        thumbnail: {
+          path: require("@/assets/car.jpg"),
+          mimetype: "image/jpeg",
+          originalname: "karelia.jpg",
+          size: 81898,
+        },
+        tank: 777,
+        colors: ["Синий", "Алый", "Белый", "Синий"],
+        id: "600ff1f6ad015e0bb6997d65",
+      },
+      color: "Синий",
+      dateFrom: 1649620800000,
+      dateTo: 1649707200000,
+      rateId: {
+        rateTypeId: {
+          unit: "мин",
+          name: "Поминутно",
+          id: "5e26a07f099b810b946c5d82",
+        },
+        price: 7,
+        id: "61af4296bb7a006c05c54c4b",
+      },
+      price: 10280,
+      isNeedChildChair: true,
+      orderStatusId: {
+        name: "Новые",
+        id: "5e26a191099b810b946c5d89",
+      },
+      id: "625321fc73b61100181024d6",
+    },
+  }),
+};
+</script>
+
+
